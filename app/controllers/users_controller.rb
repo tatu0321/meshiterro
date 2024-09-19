4d22235aa7e84a19e9a1b5bd06fd8f388fcc38f3
tatu0321 <177841065+tatu0321@users.noreply.github.com>
2024-09-19 07:56:34 +0000

    [Finish] 4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -14,7 +16,16 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
+  private
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
